d29f8baa79939b5003939d502f58e98d48e2b38e
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-14 20:45:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,20 @@ function takeANumber(line,name){
 }
 
 function nowServing(katzDeliLine){
-  
+  if(katzDeliLine.length > 0){
+    return katzDeliLine.shift()
+  }
+  else{
+    return "There is nobody waiting to be served!"
+  }
+}
+
+function currentLine(line){
+  if(line.length > 0){
+    var str = "The line is currently: "
+//
+    for(let i = 0; i < line.length;i++){
+      
+    }
+  }
 }
