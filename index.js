9abbce9534140b7d9468705596b56fc274995ce5
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-14 21:15:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 function takeANumber(katzDeliLine,name){
   katzDeliLine.push(name)
-  return `Welcome, ${name}. You are number ${katzDeliLine.length+1} in line.`
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine){
