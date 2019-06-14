554d1223d7f56c65292b9c43849fde9678c9ab92
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-14 21:00:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(line){
     for(let i = 0; i < line.length;i++){
       str = str + (i+1) + ". " + line[i] + ", "
     }
-    str = str.slice(-str.)
+    str = str.slice(-2)
   }
   else{
     str = "The line is currently empty."
